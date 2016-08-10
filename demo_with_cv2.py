96d486911909086dbd51a7f4a45b90b9d341f396
hagenschmidtchen <hagen.schmidtchen@tum.de>
2016-08-10 22:39:50 +0000

    manual mode runs with threads

--- demo_with_cv2.py
+++ demo_with_cv2.py
@@ -52,33 +52,47 @@ class manualControlThread (threading.Thread):
 		global manual_mode
 		global thread_lock
 		global thread_lock2
-		
+		global key
+		global thread_lock3
+
 		print "Starting " + self.name
 		
 		while self.running:
 			# query pressed keys
-			k = cv2.waitKey(33)
+			thread_lock3.acquire()
+			k = key
+			thread_lock3.release()
 			# escape to stop program execution
 			if k == 27: # 27=escape
 				self.running = False
 				thread_lock.acquire()
 				exiting = True
 				thread_lock.release()
+				thread_lock5.acquire()
 				drone.reset()
+				thread_lock5.release()
 			 # takeoff
 			elif k == 13: # 13=enter
 				print("return")
+				thread_lock5.acquire()
 				drone.takeoff()
+				thread_lock5.release()
 			# land
 			elif k == 32: # 32=space
 				print("space")
+				thread_lock5.acquire()
 				drone.land()
+				thread_lock5.release()
 			# emergency
 			elif k == 8: # 8=backspace
+				thread_lock5.acquire()
 				drone.reset()
+				thread_lock5.release()
 			# switch control mode
 			elif k == ord('m'):
+				thread_lock5.acquire()
 				drone.hover()
+				thread_lock5.release()
 				thread_lock2.acquire()
 				manual_mode = not manual_mode
 				thread_lock2.release()
@@ -87,50 +101,91 @@ class manualControlThread (threading.Thread):
 				# listen for additional key events for manual control
 				# forward / backward
 				if k == ord('w'):
+					thread_lock5.acquire()
 					drone.move_forward()
+					thread_lock5.release()
 				elif k == ord('s'):
+					thread_lock5.acquire()
 					drone.move_backward()
+					thread_lock5.release()
 				# left / right
 				elif k == ord('a'):
+					thread_lock5.acquire()
 					drone.move_left()
+					thread_lock5.release()
 				elif k == ord('d'):
+					thread_lock5.acquire()
 					drone.move_right()
+					thread_lock5.release()
 				# up / down
-				elif k == 2490368:
+				elif k == 2490368 or k == 65362:
+					thread_lock5.acquire()
 					drone.move_up()
-				elif k == 2621440:
+					thread_lock5.release()
+				elif k == 2621440 or k == 65364:
+					thread_lock5.acquire()
 					drone.move_down()
+					thread_lock5.release()
 				# turn left / turn right
-				elif k == 2424832:
+				elif k == 2424832 or k == 65361:
+					thread_lock5.acquire()
 					drone.turn_left()
-				elif k == 2555904:
+					thread_lock5.release()
+				elif k == 2555904 or k == 65363:
+					thread_lock5.acquire()
 					drone.turn_right()
+					thread_lock5.release()
 				# speed
 				elif k == ord('1'):
+					thread_lock5.acquire()
 					drone.speed = 0.1
+					thread_lock5.release()
 				elif k == ord('2'):
+					thread_lock5.acquire()
 					drone.speed = 0.2
+					thread_lock5.release()
 				elif k == ord('3'):
+					thread_lock5.acquire()
 					drone.speed = 0.3
+					thread_lock5.release()
 				elif k == ord('4'):
+					thread_lock5.acquire()
 					drone.speed = 0.4
+					thread_lock5.release()
 				elif k == ord('5'):
+					thread_lock5.acquire()
 					drone.speed = 0.5
+					thread_lock5.release()
 				elif k == ord('6'):
+					thread_lock5.acquire()
 					drone.speed = 0.6
+					thread_lock5.release()
 				elif k == ord('7'):
+					thread_lock5.acquire()
 					drone.speed = 0.7
+					thread_lock5.release()
 				elif k == ord('8'):
+					thread_lock5.acquire()
 					drone.speed = 0.8
+					thread_lock5.release()
 				elif k == ord('9'):
-				  drone.speed = 0.9
+					thread_lock5.acquire()
+				  	drone.speed = 0.9
+					thread_lock5.release()
 				elif k == ord('0'):
-				  drone.speed = 1.0
+					thread_lock5.acquire()
+				  	drone.speed = 1.0
+					thread_lock5.release()
 				else:
-				  drone.hover()
+					thread_lock5.acquire()
+				  	drone.hover()
+					thread_lock5.release()
+
 			  
 		print("Shutting down...")
+		thread_lock5.acquire()
 		drone.halt()
+		thread_lock5.release()
 		print("Ok.")		
 		print "Exiting " + self.name
 		
@@ -148,14 +203,17 @@ class automaticControlThread (threading.Thread):
 		global drone
 		global thread_lock
 		global thread_lock2
+		global thread_lock4
 		global exiting
 		global manual_mode
 		global W
 		global H
+		global frame_to_render
+		global new_frame_to_render
 		
 		print "Starting " + self.name
 		
-		p[0] = -W		
+		p = (-W	, 0)	
 		
 		while self.running:
 			thread_lock.acquire()
@@ -167,6 +225,7 @@ class automaticControlThread (threading.Thread):
 				
 				thread_lock2.acquire()
 				if not manual_mode:
+					thread_lock2.release()
 					# Tracking 
 					if self.status == "Start":
 						# To do with image recognition
@@ -177,7 +236,9 @@ class automaticControlThread (threading.Thread):
 						if p[0] >= -W/2:
 							self.status = "Tracking"
 						else:
+							thread_lock5.acquire()
 							drone.turn_left()
+							thread_lock5.release()
 						print 'Searching'
 					elif self.status == "Tracking":
 						# To do image recognition and controler calculation
@@ -187,19 +248,47 @@ class automaticControlThread (threading.Thread):
 						else:
 							self.status = "Searching"
 						print 'tracking'
-					# Controler
+				else:
+					thread_lock2.release()
+					try:
+						# print pygame.image
+						thread_lock5.acquire()
+						pixelarray = drone.get_image()
+						thread_lock5.release()
+						if pixelarray != None:
+							frame = pixelarray[:,:,::-1].copy()
+							#resize image
+							resized=cv2.resize(frame,(W,H))
+
+						#image conversion
+
+						# battery status
+						#	hud_color = (255, 0, 0) if drone.navdata.get('drone_state', dict()).get('emergency_mask', 1) else (10, 10, 255)
+						#	bat = drone.navdata.get(0, dict()).get('battery', 0)
+						# f = pygame.font.Font(None, 20)
+						# hud = f.render('Battery: %i%%' % bat, True, hud_color) 
+						# screen.blit(hud, (10, 10))
+
+							thread_lock4.acquire()
+							frame_to_render = frame
+							new_frame_to_render = True
+							thread_lock4.release()
+
+					except:
+						pass
 				
 		print "Exiting" + self.name
 		
 	def getAndSearchImage(self):
 		 try:
 			# print pygame.image
+			thread_lock5.acquire()
 			pixelarray = drone.get_image()
+			thread_lock5.release()
 			if pixelarray != None:
 				frame = pixelarray[:,:,::-1].copy()
 			#resize image
 				resized=cv2.resize(frame,(W,H))
-				print 'got image'
 			# aruco detection
 				gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 				aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
@@ -211,12 +300,17 @@ class automaticControlThread (threading.Thread):
 				gray = aruco.drawDetectedMarkers(gray, corners)
 
 				# battery status
-				hud_color = (255, 0, 0) if drone.navdata.get('drone_state', dict()).get('emergency_mask', 1) else (10, 10, 255)
-				bat = drone.navdata.get(0, dict()).get('battery', 0)
+				#hud_color = (255, 0, 0) if drone.navdata.get('drone_state', dict()).get('emergency_mask', 1) else (10, 10, 255)
+				#bat = drone.navdata.get(0, dict()).get('battery', 0)
 				# f = pygame.font.Font(None, 20)
 				# hud = f.render('Battery: %i%%' % bat, True, hud_color) 
 				# screen.blit(hud, (10, 10))
-				cv2.imshow('Drone',gray)			
+
+				thread_lock4.acquire()
+				frame_to_render = gray
+				new_frame_to_render = True
+				thread_lock4.release()
+
 				x = 1
 				y = 2
 				hx = 3
@@ -261,11 +355,15 @@ class automaticControlThread (threading.Thread):
 		ux = K_p * (ex + uix + udx)
 		
 		if ux < -ux_threshold :
+			thread_lock5.acquire()
 			drone.speed = -MAX_SPEED_ROT * ux
 			drone.turn_right()
+			thread_lock5.release()
 		elif ux > +ux_threshold :
+			thread_lock5.acquire()
 			drone.speed = MAX_SPEED_ROT * ux
 			drone.turn_left()
+			thread_lock5.release()
 		
 		K_py=2.0
 		K_dy=0.0
@@ -295,26 +393,36 @@ class automaticControlThread (threading.Thread):
 		uy = K_py * (ey + uiy + udy)
 		
 		if uy < -uy_threshold :
+			thread_lock5.acquire()
 			drone.speed = -MAX_SPEED_ROT * uy
 			drone.turn_up()
+			thread_lock5.release()
 		elif uy > +uy_threshold :
+			thread_lock5.acquire()
 			drone.speed = MAX_SPEED_ROT * uy
 			drone.turn_down()
+			thread_lock5.release()
 		 
 
 def main():
 	global drone
 	global thread_lock
 	global thread_lock2
+	global thread_lock3
+	global thread_lock4
+	global thread_lock5
 	global exiting
 	global manual_mode
 	global W
 	global H
+	global key
+	global frame_to_render
+	global new_frame_to_render
 	W, H = 320, 240
+	new_frame_to_render = False
+	key = -1
 	
 	
-	drone = libardrone.ARDrone(True)
-	drone.reset()
 	
 	exiting = False
 	manual_mode = True
@@ -322,11 +430,18 @@ def main():
 	threads = []
 	thread_lock = threading.Lock()
 	thread_lock2 = threading.Lock()
+	thread_lock3 = threading.Lock()
+	thread_lock4 = threading.Lock()
+	thread_lock5 = threading.Lock()
 
-	
+	thread_lock5.acquire()
+	drone = libardrone.ARDrone(True)
+	drone.reset()
+	thread_lock5.release()
+
 	# Create new threads
-	manual_control_thread = manualControlThread(1, "Manual Control Thread", 1)
-	automatic_control_thread = automaticControlThread(2, "Automatic Control Thread", 2)
+	manual_control_thread = manualControlThread(1, "Manual Control Thread")
+	automatic_control_thread = automaticControlThread(2, "Automatic Control Thread")
 	
 	# Start new Threads
 	manual_control_thread.start()
@@ -336,6 +451,16 @@ def main():
 	threads.append(manual_control_thread)
 	threads.append(automatic_control_thread)
 	
+	while not exiting:
+		thread_lock3.acquire()
+		key = cv2.waitKey(33)
+		thread_lock3.release()
+		
+		thread_lock4.acquire()
+		if new_frame_to_render:
+			cv2.imshow('Drone',frame_to_render)
+			new_frame_to_render = False
+		thread_lock4.release()			
 	# Wait for all threads to complete
 	for t in threads:
 		t.join()
